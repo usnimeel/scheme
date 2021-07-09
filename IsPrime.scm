92ec2ab6ee2c7faaaed5400e534a18d117e2f386
Minsu Lee <mn394631@dal.ca>
2021-07-09 17:33:03 +0900

    A prime number checker

---
+++ IsPrime.scm
@@ -0,0 +1,27 @@
+(load "io.scm")
+
+; determine n is a prime or not.
+(define isPrime (lambda (n)
+        (if (< n 2) ; 0 and 1 are not a prime number.
+            #f
+            (isPrime-helper n (- n 1))
+        )    
+    )
+)
+
+(define isPrime-helper (lambda (n d)
+        (if (equal? (remainder n d) 0)
+            #f ; A remainder of n equals zero, which means that there is a divisible value for n.
+            (if (> d 2) ; stopper (until divided by 2)
+                (isPrime-helper n (- d 1))
+                #t ; n has a non-zero remainder until divided by 2.
+            )
+        )
+    )
+)
+
+(display "Enter a number: ")
+(define n (getline))
+(display (isPrime (string->number n)))
+(newline)
+(exit)
