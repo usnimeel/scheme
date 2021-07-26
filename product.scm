64d919d0c31587c21156fda5ec7f0e8b662abfbc
Minsu Lee <usnimeel@gmail.com>
2021-07-26 19:51:00 +0900

    Add: tail recursive

---
+++ product.scm
@@ -0,0 +1,25 @@
+
+;(define product (lambda (L)
+;(if (null? L)
+;  1
+;  (* (product (cdr L)) (car L))
+;)
+;))
+; Rewrite the above code as a tail-recursive function
+
+(define product-tr (lambda (L)
+  (letrec ((product-helper (lambda (partial lst)
+              (if (null? lst)
+                  ;  1
+                  ;  (* (product (cdr L)) (car L))
+                  partial
+                  (product-helper (* partial (car lst)) (cdr lst))
+              )
+            ))
+          )
+    (product-helper 1 L)
+  )
+))
+
+(product-tr `(1 2 3 4 5))
+(exit)
