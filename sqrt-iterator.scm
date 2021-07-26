1e6ff32f0035930091db1701413b37ee62bccc8d
Minsu Lee <usnimeel@gmail.com>
2021-07-26 20:00:27 +0900

    Add: find square root in a list

---
+++ sqrt-iterator.scm
@@ -0,0 +1,23 @@
+; Implement a function that takes a list of numbers as an argument 
+; and returns an iterator that returns the square root
+
+(define new-sqrt-iterator (lambda (lst)
+  ; clouser
+  (lambda ()
+    (if (null? lst)
+      #f                      ; no more argument
+      (let ((top (car lst)))  ; store first value of list into top
+        (set! lst (cdr lst))
+        (sqrt top)
+      )     
+    )
+  )                  
+))
+
+(define sql-test (new-sqrt-iterator `(1 2 3 4)))
+(sql-test)
+(sql-test)
+(sql-test)
+(sql-test)
+(sql-test)
+(exit)
